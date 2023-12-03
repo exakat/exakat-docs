fc5293b865cd32bc1398032e0efa968cbde2b102
Damien Seguy <dseguy@exakat.io>
2023-12-03 17:24:48 +0100

    added google search verification

--- conf.py
+++ conf.py
@@ -291,3 +291,5 @@ from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
 lexers["php"] = PhpLexer(startinline=True, linenos=1)
 lexers["php-annotations"] = PhpLexer(startinline=True, linenos=1)
+
+html_extra_path = ["googlee919cb0917e4fefc.html"]
